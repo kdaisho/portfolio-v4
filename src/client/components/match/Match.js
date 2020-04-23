611433c0c19e1eccf823412162f222954ba6ba37
daisho <daishokomiyama@gmail.com>
2020-04-22 22:53:04 -0400

    Making work on touch device. Need to figure out to passive flag

--- src/client/components/match/Match.js
+++ src/client/components/match/Match.js
@@ -3,11 +3,16 @@ import "./match.css";
 
 class Match extends Component {
     state = {
-        lastPosition: "",
         tags: [],
         currentTag: "",
         indexFrom: "",
-        indexTo: ""
+        indexTo: "",
+        coordinates: [],
+        lastPosition: 0,
+        wasInside: false,
+        lastSpot: "",
+        hasMoved: false,
+        unlocked: false
     };
 
     list = [
@@ -41,10 +46,11 @@ class Match extends Component {
 
     componentDidMount() {
         this.init();
-        // this.initTouch();
+        this.initTouch();
     }
 
     setOrder = () => {
+        console.log("%cSET ORDER", "color: purple");
         this.state.lastPosition &&
             this.spotsRef[this.state.lastPosition].classList.remove("hovered");
         this.setState({ tags: document.querySelectorAll(".tag") }, () =>
@@ -101,7 +107,12 @@ class Match extends Component {
         });
     };
 
-    getPosIndex = () => {
+    getPosIndex = type => {
+        console.log(
+            `%cPARENT POSITION ${type}`,
+            "color:red",
+            event.target.parentElement.dataset.position
+        );
         return parseInt(event.target.parentElement.dataset.position, 10);
     };
 
@@ -153,114 +164,145 @@ class Match extends Component {
     };
 
     // ********** TOUCH ***********
-    // getPosition = (x, y) => {
-    //     for (let i = 0; i < this.state.coordinates.length; i++) {
-    //         if (
-    //             x >= this.state.coordinates[i].x &&
-    //             x <= this.state.coordinates[i].right &&
-    //             y >= this.state.coordinates[i].y &&
-    //             y <= this.state.coordinates[i].bottom
-    //         ) {
-    //             return i;
-    //         }
-    //     }
-    // };
+    getPosition = (x, y) => {
+        for (let i = 0; i < this.state.coordinates.length; i++) {
+            if (
+                x >= this.state.coordinates[i].x &&
+                x <= this.state.coordinates[i].right &&
+                y >= this.state.coordinates[i].y &&
+                y <= this.state.coordinates[i].bottom
+            ) {
+                return i;
+            }
+        }
+    };
 
-    // touchStart = event => {
-    //     this.state.indexFrom = this.getPosition(
-    //         event.targetTouches[0].pageX,
-    //         event.targetTouches[0].pageY
-    //     );
-    //     this.state.lastPosition = parseInt(
-    //         event.target.parentElement.getAttribute("data-position")
-    //     );
-    //     this.state.initialX = event.targetTouches[0].pageX;
-    //     this.state.initialY = event.targetTouches[0].pageY;
-    // };
+    touchStart = index => {
+        console.log("EVENT:", index, event.target);
+        this.setState(
+            {
+                indexFrom: this.getPosition(
+                    event.targetTouches[0].pageX,
+                    event.targetTouches[0].pageY
+                )
+                // indexFrom: this.getPosIndex("Start")
+            },
+            () => {
+                console.log("INDEX FROM:", this.state.indexFrom);
+                // this.disableTags(index);
+                this.disableTags(this.state.indexFrom);
+            }
+        );
+        //this.state.lastPosition = parseInt(
+        // event.target.parentElement.getAttribute("data-position")
+        //);
+        // console.log("POS INdex:", this.getPosIndex());
+        this.setState(
+            {
+                lastPosition: this.getPosIndex("Start 2"),
+                // lastPosition: this.getPosition(
+                //     event.targetTouches[0].pageX,
+                //     event.targetTouches[0].pageY
+                // ),
+                initialX: event.targetTouches[0].pageX,
+                initialY: event.targetTouches[0].pageY
+            },
+            () => this.setState({ unlocked: true })
+        );
+        // const ha = this.removeUpDownFromTags();
+        // console.log("HA?", ha);
+    };
 
-    // touchMove = event => {
-    //     if (event.cancelable) event.preventDefault();
-    //     if (!this.state.hasMoved) {
-    //         this.state.hasMoved = true;
-    //         event.target.style.zIndex = 100;
-    //     }
-    //     event.target.style.transform = `translate(${
-    //         event.targetTouches[0].pageX - this.state.initialX
-    //     }px, ${event.targetTouches[0].pageY - this.state.initialY}px)`;
-    //     this.state.indexTo = this.getPosition(
-    //         event.targetTouches[0].pageX,
-    //         event.targetTouches[0].pageY
-    //     );
-    //     if (
-    //         typeof this.state.indexFrom === "number" &&
-    //         typeof this.state.indexTo === "number"
-    //     ) {
-    //         if (!this.state.wasInside) {
-    //             this.state.lastSpot = this.state.indexTo;
-    //             this.state.spots[this.state.indexTo] &&
-    //                 this.state.spots[this.state.indexTo].classList.add(
-    //                     "hovered"
-    //                 );
-    //             this.pushTags(this.state.indexFrom < this.state.indexTo);
+    touchMove = index => {
+        if (event.cancelable) event.preventDefault();
+        if (this.state.unlocked === false) return;
+        if (!this.state.hasMoved) {
+            // this.state.hasMoved = true;
+            this.setState({ hasMoved: true });
+            // event.target.style.zIndex = 100;
+            this.state.tags[this.state.lastPosition].style.zIndex = 100;
+        }
+        // event.target.style.transform = `translate(${
+        //     event.targetTouches[0].pageX - this.state.initialX
+        // }px, ${event.targetTouches[0].pageY - this.state.initialY}px)`;
+        this.state.tags[this.state.lastPosition].style.transform = `translate(${
+            event.targetTouches[0].pageX - this.state.initialX
+        }px, ${event.targetTouches[0].pageY - this.state.initialY}px)`;
+        this.setState(
+            {
+                indexTo: this.getPosition(
+                    event.targetTouches[0].pageX,
+                    event.targetTouches[0].pageY
+                )
+            },
+            () => {
+                console.log(
+                    "NOT NUMBER?:",
+                    this.state.indexFrom,
+                    this.state.indexTo
+                );
+                if (
+                    typeof this.state.indexFrom === "number" &&
+                    typeof this.state.indexTo === "number"
+                ) {
+                    if (!this.state.wasInside) {
+                        this.state.lastSpot = this.state.indexTo;
+                        this.spotsRef[this.state.indexTo] &&
+                            this.spotsRef[this.state.indexTo].classList.add(
+                                "hovered"
+                            );
+                        this.pushTags(
+                            this.state.indexFrom < this.state.indexTo
+                        );
+                        this.setState({ wasInside: true });
+                    }
+                } else {
+                    if (this.state.wasInside) {
+                        this.spotsRef[this.state.lastSpot] &&
+                            this.spotsRef[this.state.lastSpot].classList.remove(
+                                "hovered"
+                            );
+                        this.removeUpDownFromTags();
+                        this.setState({ wasInside: false });
+                    }
+                }
+            }
+        );
+    };
 
-    //             // this.state.wasInside = true;
-    //             this.setState({ wasInside: true });
-    //         }
-    //     } else {
-    //         if (this.state.wasInside) {
-    //             this.state.spots[this.state.lastSpot] &&
-    //                 this.state.spots[this.state.lastSpot].classList.remove(
-    //                     "hovered"
-    //                 );
-    //             this.removeUpDownFromTags();
-    //             // this.state.wasInside = false;
-    //             this.setState({ wasInside: false });
-    //         }
-    //     }
-    // };
+    touchEnd = index => {
+        console.log("END index:", index);
+        if (!this.state.hasMoved) return false;
+        const refreshed = this.removeUpDownFromTags();
+        if (typeof this.state.indexTo === "number" && refreshed) {
+            this.spotsRef[this.state.indexTo] &&
+                this.spotsRef[this.state.indexTo].classList.remove("hovered");
+            this.dropTags(this.state.indexTo);
+            // this.spotsRef[this.state.indexTo].append(this.state.tags[index]);
+            this.spotsRef[this.state.indexTo].append(
+                this.state.tags[this.state.lastPosition]
+            );
+            // this.state.hasMoved = false;
+            this.setState({ hasMoved: false });
+        }
+        requestAnimationFrame(() => {
+            // event.target.removeAttribute("style");
+            this.enableTags();
+            this.state.tags[this.state.lastPosition].removeAttribute("style");
+            this.setOrder();
+            this.setState({ unlocked: false });
+        });
+    };
 
-    // touchEnd = event => {
-    //     if (!this.state.hasMoved) return false;
-    //     this.removeUpDownFromTags();
-    //     if (typeof this.state.indexTo === "number") {
-    //         this.state.spots[this.state.indexTo] &&
-    //             this.state.spots[this.state.indexTo].classList.remove(
-    //                 "hovered"
-    //             );
-    //         this.dropTags(this.state.indexTo);
-    //         this.state.spots[this.state.indexTo].append(this);
-    //         // this.state.hasMoved = false;
-    //         this.setState({ hasMoved: false });
-    //     }
-    //     requestAnimationFrame(() => {
-    //         event.target.removeAttribute("style");
-    //         this.setOrder();
-    //     });
-    // };
-
-    // initTouch = () => {
-    //     this.state.coordinates = [];
-    //     this.state.lastPosition = 0;
-    //     this.state.wasInside = false;
-    //     this.state.lastSpot = "";
-    //     this.state.hasMoved = false;
-
-    //     for (let i = 0; i < this.state.spots.length; i++) {
-    //         this.state.coordinates.push(
-    //             this.state.spots[i].getBoundingClientRect()
-    //         );
-    //     }
-    //     for (let i = 0; i < this.state.tags.length; i++) {
-    //         this.state.tags[i].addEventListener("touchstart", this.touchStart);
-    //         this.state.tags[i].addEventListener("touchmove", this.touchMove, {
-    //             passive: false
-    //         });
-    //         this.state.tags[i].addEventListener(
-    //             "touchend",
-    //             this.state.touchEnd
-    //         );
-    //     }
-    // };
+    initTouch = () => {
+        const coordinates = [];
+        for (let i = 0; i < this.spotsRef.length; i++) {
+            coordinates.push(this.spotsRef[i].getBoundingClientRect());
+        }
+        this.setState({ coordinates }, () =>
+            console.log("COORDINATES:", this.state.coordinates)
+        );
+    };
 
     // window.addEventListener("resize", () => {
     //     this.state.init();
@@ -299,6 +341,9 @@ class Match extends Component {
                                 draggable="true"
                                 onDragStart={() => this.dragStart(item.id)}
                                 onDragEnd={() => this.dragEnd()}
+                                onTouchStart={() => this.touchStart(item.id)}
+                                onTouchMove={() => this.touchMove(item.id)}
+                                onTouchEnd={() => this.touchEnd(item.id)}
                                 data-element={item.id}
                             >
                                 <span className="text">{item.text}</span>

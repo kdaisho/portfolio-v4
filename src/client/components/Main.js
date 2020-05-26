44d2d386e700a1fb1693c4570d87b2be01e15003
daisho <daishokomiyama@gmail.com>
2020-05-26 08:56:58 -0400

    Adding mobile menu

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./header/Header";
 import Hero from "./hero/Hero";
 import Education from "./education/Education";
@@ -7,18 +7,31 @@ import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
 
-const Main = () => {
-    return (
-        <React.Fragment>
-            <Header />
-            <Hero />
-            <Education />
-            <Technologies />
-            <Projects />
-            <Contact />
-            <Footer />
-        </React.Fragment>
-    );
-};
+class Main extends Component {
+    state = {
+        mobileMenuOpen: false
+    };
+
+    toggleState = (state) => {
+        console.log("ST", state);
+        this.setState((prevState) => ({
+            [state]: !prevState[state]
+        }));
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Header toggleState={this.toggleState} mobileMenuOpen={this.state.mobileMenuOpen} />
+                <Hero />
+                <Education />
+                <Technologies />
+                <Projects />
+                <Contact />
+                <Footer />
+            </React.Fragment>
+        );
+    }
+}
 
 export default Main;
--- src/client/components/header/Header.js
+++ src/client/components/header/Header.js
@@ -4,37 +4,38 @@ import Logo from "../../images/nav/Logo";
 import menu from "../../images/nav/menu-dots.svg";
 import "./header.css";
 
-const Header = () => {
-    return (
-        <header className="header">
-            <div className="content-wrap nav">
-                <div className="nav-left">
-                    <div className="logo">
-                        <Logo />
-                    </div>
-                </div>
-                <div id="navRight" className="nav-right">
-                    <ul className="social-links">
-                        {socials.map((item) => (
-                            <li key={item.name}>
-                                <img src={item.src} alt={item.name} />
-                            </li>
-                        ))}
-                    </ul>
-                    <button className="menu-toggle">
-                        <img src={menu} alt="menu" />
-                    </button>
-                    <div className="menu-pane">
-                        {menuItems.map((item) => (
-                            <button key={item.name} className="button is-medium has-shadow">
-                                {item.name}
-                            </button>
-                        ))}
-                    </div>
+const Header = (props) => (
+    <header className="header">
+        <div className="content-wrap nav">
+            <div className="nav-left">
+                <div className="logo">
+                    <Logo />
                 </div>
             </div>
-        </header>
-    );
-};
+            <div id="navRight" className="nav-right">
+                <ul className="social-links">
+                    {socials.map((item) => (
+                        <li key={item.name}>
+                            <img src={item.src} alt={item.name} />
+                        </li>
+                    ))}
+                </ul>
+                <button className="menu-toggle" onClick={() => props.toggleState("mobileMenuOpen")}>
+                    <img src={menu} alt="menu" />
+                </button>
+                <div className={`menu-pane ${props.mobileMenuOpen ? "active" : ""}`}>
+                    {menuItems.map((item) => (
+                        <button
+                            key={item.name}
+                            className="button is-medium has-shadow is-menu-button"
+                        >
+                            {item.name}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </header>
+);
 
 export default Header;

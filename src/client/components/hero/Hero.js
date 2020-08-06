0fbc995c84fcf7c15e48d53824530371e1be67e8
daisho <daishokomiyama@gmail.com>
2020-08-06 16:23:21 -0400

    Converting classes into functions

--- src/client/components/hero/Hero.js
+++ src/client/components/hero/Hero.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import cat from "../../images/hero/cat-body-opt.svg";
 import catAction from "../../images/hero/cat-actions-opt.svg";
 import "./hero.css";
 
-class Hero extends Component {
-    state = {
-        animation: true,
-        w: window.innerWidth
-    };
-
-    options = {
+const Hero = () => {
+    const [animation, setAnimation] = useState(true);
+    const [innerWidth, setInnerWidth] = useState(0);
+    const options = {
         passive: true,
         capture: true
     };
 
-    componentDidMount() {
-        setTimeout(() => this.setState({ animation: false }), 5000);
-        addEventListener("load", this.handleResize, this.options);
-        addEventListener("resize", this.handleResize, this.options);
-    }
+    useEffect(() => {
+        setTimeout(() => setAnimation(false), 5000);
+        addEventListener("load", handleResize, options);
+        addEventListener("resize", handleResize, options);
 
-    componentWillUnmount() {
-        window.removeEventListener("load", this.handleResize, this.options);
-        window.removeEventListener("resize", this.handleResize, this.options);
-    }
+        return () => {
+            window.removeEventListener("load", handleResize, options);
+            window.removeEventListener("resize", handleResize, options);
+        };
+    });
 
-    handleResize = () => {
-        this.setState({ w: window.innerWidth });
+    const handleResize = () => {
+        setInnerWidth(window.innerWidth);
     };
 
-    renderGreetings = () => {
-        const years = new Date().getFullYear() - 2014;
+    const renderGreetings = () => {
         return (
-            <div className={`greetings-wrap ${this.state.animation ? "slide" : ""}`}>
+            <div className={`greetings-wrap ${animation ? "slide" : ""}`}>
                 <div className="clip-path"></div>
                 <p className="greetings">
                     Hi, my name’s Daisho Komiyama and I'm a front-end developer based in Montreal.
-                    I’ve been a developer for about 6 years. Passionate about solving problems and
-                    building tools that makes our lives easier. I'm an effective developer who ships
-                    code regularly.
+                    I’ve been a developer for about {new Date().getFullYear() - 2014} years.
+                    Passionate about solving problems and building tools that makes our lives
+                    easier. I'm an effective developer who ships code regularly.
                 </p>
             </div>
         );
     };
 
-    render() {
-        return (
-            <section id="pageTop" className="is-hero">
-                <div className="content-wrap is-side-by-side">
-                    <div className="left-side">
-                        <div className="typewriter">
-                            <h1>Who am I?</h1>
-                        </div>
+    return (
+        <section id="pageTop" className="is-hero">
+            <div className="content-wrap is-side-by-side">
+                <div className="left-side">
+                    <div className="typewriter">
+                        <h1>Who am I?</h1>
                     </div>
-                    <div className="right-side" style={{ backgroundImage: `url(${cat})` }}>
-                        <div
-                            className="sequence"
-                            style={{ background: `url(${catAction}) 0 0 no-repeat` }}
-                        ></div>
-                        {this.state.w >= 769 && this.renderGreetings()}
-                    </div>
-                    {this.state.w <= 768 && this.renderGreetings()}
                 </div>
-            </section>
-        );
-    }
-}
+                <div className="right-side" style={{ backgroundImage: `url(${cat})` }}>
+                    <div
+                        className="sequence"
+                        style={{ background: `url(${catAction}) 0 0 no-repeat` }}
+                    ></div>
+                    {innerWidth >= 769 && renderGreetings()}
+                </div>
+                {innerWidth <= 768 && renderGreetings()}
+            </div>
+        </section>
+    );
+};
 
 export default Hero;
--- src/client/components/projects/Projects.js
+++ src/client/components/projects/Projects.js
@@ -1,42 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { projects, filterItems } from "./projects-data.js";
 import Modal from "../modal/Modal";
 import { Desktop, Github } from "../../svg/Icons";
 import "./projects.css";
 
-class Projects extends Component {
-    state = {
-        filterTerms: [],
-        angular: false,
-        cssGrid: false,
-        less: false,
-        live: false,
-        mongodb: false,
-        nodejs: false,
-        php: false,
-        react: false,
-        sass: false,
-        vanillajs: false,
-        selectedProject: {},
-        activeCardId: ""
-    };
+const Projects = (props) => {
+    const [filterTerms, setFilterTerms] = useState([]);
+    const [selectedProject, setSelectedProject] = useState({});
+    const [activeCardId, setActiveCardId] = useState("");
 
-    handleChange = (tech) => {
-        const name = event.target.name;
-        this.setState({ [name]: event.target.checked });
-        const copy = [...this.state.filterTerms];
+    const handleChange = (tech) => {
+        const copy = [...filterTerms];
         if (event.target.checked) {
-            this.setState((prevState) => ({ filterTerms: [...prevState.filterTerms, tech] }));
+            setFilterTerms((prevFilterTerms) => [...prevFilterTerms, tech]);
         } else {
-            const index = this.state.filterTerms.indexOf(tech);
+            const index = filterTerms.indexOf(tech);
             if (index >= 0) {
                 copy.splice(index, 1);
-                this.setState({ filterTerms: copy });
+                setFilterTerms(copy);
             }
         }
     };
 
-    isIncludes = (stack, filterTerms) => {
+    const isIncludes = (stack, filterTerms) => {
         let counter = 0;
         for (let i = 0; i < filterTerms.length; i++) {
             stack.includes(filterTerms[i]) ? counter++ : "";
@@ -44,175 +30,163 @@ class Projects extends Component {
         return filterTerms.length === counter ? true : false;
     };
 
-    toggleModal = ({ id, title, subtitle, description, hero, stack, url, githubUrl }, delay) => {
-        this.setState({ activeCardId: id }, () => {
-            setTimeout(() => {
-                this.setState({
-                    selectedProject: Object.keys(this.state.selectedProject).length
-                        ? {}
-                        : {
-                              title,
-                              subtitle,
-                              description,
-                              hero,
-                              stack,
-                              url,
-                              githubUrl
-                          }
-                });
-            }, delay);
-        });
+    const toggleModal = (
+        { id, title, subtitle, description, hero, stack, url, githubUrl },
+        delay
+    ) => {
+        setActiveCardId(id);
+        setTimeout(() => {
+            setSelectedProject(
+                Object.keys(selectedProject).length
+                    ? {}
+                    : { title, subtitle, description, hero, stack, url, githubUrl }
+            );
+        }, delay);
     };
 
-    render() {
-        const { selectedProject } = this.state;
-        return (
-            <section id="toSideProjects" className="section is-projects">
-                <div className="content-wrap is-side-by-side">
-                    <div className="right-side">
-                        <div className="title-group has-filters">
-                            <h2 className="title">Side Projects</h2>
-                            <p className="subtitle font-large">
-                                Getting my hands dirty with technologies I don't have a chance to
-                                work with in my day job helps me stay up to date.
-                            </p>
-                        </div>
-                        <fieldset
-                            className={`filter-section ${
-                                this.props.openPane === "projects" ? "active" : ""
-                            }`}
+    return (
+        <section id="toSideProjects" className="section is-projects">
+            <div className="content-wrap is-side-by-side">
+                <div className="right-side">
+                    <div className="title-group has-filters">
+                        <h2 className="title">Side Projects</h2>
+                        <p className="subtitle font-large">
+                            Getting my hands dirty with technologies I don't have a chance to work
+                            with in my day job helps me stay up to date.
+                        </p>
+                    </div>
+                    <fieldset
+                        className={`filter-section ${
+                            props.openPane === "projects" ? "active" : ""
+                        }`}
+                    >
+                        <button
+                            className="toggle-filter outline-button"
+                            onClick={() => props.togglePane("projects")}
                         >
-                            <button
-                                className="toggle-filter outline-button"
-                                onClick={() => this.props.togglePane("projects")}
-                            >
-                                Filters
-                            </button>
-                            <legend>(AND) Filters</legend>
-                            <div className="filters">
-                                {filterItems.map((item) => (
-                                    <label
-                                        key={item.tech}
-                                        className={`tag ${this.state[item.tech] ? "active" : ""}`}
-                                    >
-                                        <input
-                                            type="checkbox"
-                                            name={item.tech}
-                                            checked={this.state.tech}
-                                            onChange={() => this.handleChange(item.tech)}
-                                        />
-                                        <span className="dummy"></span>
-                                        {item.name}
-                                    </label>
-                                ))}
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div className="cards left-side">
-                        {projects
-                            .filter((project) => {
-                                return this.state.filterTerms.length
-                                    ? this.isIncludes(project.stack, this.state.filterTerms)
-                                    : true;
-                            })
-                            .map((project) => (
-                                <div
-                                    key={project.id}
-                                    className={`card ${
-                                        this.state.activeCardId === project.id ? "active" : ""
+                            Filters
+                        </button>
+                        <legend>(AND) Filters</legend>
+                        <div className="filters">
+                            {filterItems.map((item) => (
+                                <label
+                                    key={item.tech}
+                                    className={`tag ${
+                                        filterTerms.includes(item.tech) ? "active" : ""
                                     }`}
-                                    onClick={() => this.toggleModal(project, 100)}
                                 >
-                                    <div
-                                        className="top"
-                                        style={{
-                                            background: `#444 url(${project.thumb}) no-repeat center`,
-                                            backgroundSize: "cover"
-                                        }}
-                                    >
-                                        <div className="glass"></div>
-                                    </div>
-
-                                    <div className="bottom">
-                                        <div className="text-group">
-                                            <h2 className="title">{project.title}</h2>
-                                            <span className="separator"></span>
-                                            <p className="desc">{project.subtitle}</p>
-                                        </div>
-                                    </div>
-                                </div>
+                                    <input
+                                        type="checkbox"
+                                        name={item.tech}
+                                        onChange={() => handleChange(item.tech)}
+                                    />
+                                    <span className="dummy"></span>
+                                    {item.name}
+                                </label>
                             ))}
-                    </div>
-                    {!!Object.keys(selectedProject).length && (
-                        <Modal>
-                            <div className="backdrop" onClick={this.toggleModal}>
+                        </div>
+                    </fieldset>
+                </div>
+                <div className="cards left-side">
+                    {projects
+                        .filter((project) => {
+                            return filterTerms.length
+                                ? isIncludes(project.stack, filterTerms)
+                                : true;
+                        })
+                        .map((project) => (
+                            <div
+                                key={project.id}
+                                className={`card ${activeCardId === project.id ? "active" : ""}`}
+                                onClick={() => toggleModal(project, 100)}
+                            >
                                 <div
-                                    className="project-details bit-style active"
-                                    onClick={(event) => event.stopPropagation()}
+                                    className="top"
+                                    style={{
+                                        background: `#444 url(${project.thumb}) no-repeat center`,
+                                        backgroundSize: "cover"
+                                    }}
                                 >
-                                    <div
-                                        className="top"
-                                        style={{
-                                            background: `#666 url(${selectedProject.hero})`,
-                                            backgroundSize: "cover"
-                                        }}
-                                    ></div>
-                                    <div className="bottom">
-                                        <div className="text-group">
-                                            <h1 className="title">{selectedProject.title}</h1>
-                                            <p className="subtitle">{selectedProject.subtitle}</p>
-                                            <p className="description">
-                                                {selectedProject.description}
-                                            </p>
-                                            <div className="links">
-                                                {selectedProject.url && (
-                                                    <a
-                                                        href={selectedProject.url}
-                                                        title={selectedProject.title}
-                                                        className="link is-desktop-icon"
-                                                        target="_blank"
-                                                        rel="noopener noreferrer"
-                                                    >
-                                                        {Desktop()} Visit The Website
-                                                    </a>
-                                                )}
-                                                {selectedProject.githubUrl && (
-                                                    <a
-                                                        href={selectedProject.githubUrl}
-                                                        title={`${selectedProject.title} github repository`}
-                                                        className="link is-github-icon"
-                                                        target="_blank"
-                                                        rel="noopener noreferrer"
-                                                    >
-                                                        {Github()}View Github Repo
-                                                    </a>
-                                                )}
-                                            </div>
-                                        </div>
-                                        <div className="tech-stack">
-                                            {selectedProject.stack.map((tech) => {
-                                                return tech === "live" ? (
-                                                    false
-                                                ) : (
-                                                    <span key={tech} className="tech">
-                                                        {tech}
-                                                    </span>
-                                                );
-                                            })}
-                                        </div>
+                                    <div className="glass"></div>
+                                </div>
 
-                                        <button className="close" onClick={this.toggleModal}>
-                                            &#10005;
-                                        </button>
+                                <div className="bottom">
+                                    <div className="text-group">
+                                        <h2 className="title">{project.title}</h2>
+                                        <span className="separator"></span>
+                                        <p className="desc">{project.subtitle}</p>
                                     </div>
                                 </div>
                             </div>
-                        </Modal>
-                    )}
+                        ))}
                 </div>
-            </section>
-        );
-    }
-}
+                {!!Object.keys(selectedProject).length && (
+                    <Modal>
+                        <div className="backdrop" onClick={toggleModal}>
+                            <div
+                                className="project-details bit-style active"
+                                onClick={(event) => event.stopPropagation()}
+                            >
+                                <div
+                                    className="top"
+                                    style={{
+                                        background: `#666 url(${selectedProject.hero})`,
+                                        backgroundSize: "cover"
+                                    }}
+                                ></div>
+                                <div className="bottom">
+                                    <div className="text-group">
+                                        <h1 className="title">{selectedProject.title}</h1>
+                                        <p className="subtitle">{selectedProject.subtitle}</p>
+                                        <p className="description">{selectedProject.description}</p>
+                                        <div className="links">
+                                            {selectedProject.url && (
+                                                <a
+                                                    href={selectedProject.url}
+                                                    title={selectedProject.title}
+                                                    className="link is-desktop-icon"
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    {Desktop()} Visit The Website
+                                                </a>
+                                            )}
+                                            {selectedProject.githubUrl && (
+                                                <a
+                                                    href={selectedProject.githubUrl}
+                                                    title={`${selectedProject.title} github repository`}
+                                                    className="link is-github-icon"
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    {Github()}View Github Repo
+                                                </a>
+                                            )}
+                                        </div>
+                                    </div>
+                                    <div className="tech-stack">
+                                        {selectedProject.stack.map((tech) => {
+                                            return tech === "live" ? (
+                                                false
+                                            ) : (
+                                                <span key={tech} className="tech">
+                                                    {tech}
+                                                </span>
+                                            );
+                                        })}
+                                    </div>
+
+                                    <button className="close" onClick={toggleModal}>
+                                        &#10005;
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Modal>
+                )}
+            </div>
+        </section>
+    );
+};
 
 export default Projects;

2af70c32537c1958934d846747a110be1c6e52f1
daisho <daishokomiyama@gmail.com>
2020-08-07 12:18:18 -0400

    Converting App into function component

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import WorkLog from "./components/workLog/WorkLog";
@@ -8,45 +8,32 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import "./app.css";
 
-class App extends Component {
-    state = {
-        openPane: ""
+const App = () => {
+    const [openPane, setOpenPane] = useState("");
+
+    const togglePane = (name) => {
+        setOpenPane(this.state.openPane ? "" : name);
     };
 
-    togglePane = (name) => {
-        this.setState({ openPane: this.state.openPane ? "" : name });
-    };
-
-    scrollTo = (destinationId) => {
+    const scrollTo = (destinationId) => {
         const button = event.target;
         button.classList.add("active");
         setTimeout(() => button.classList.remove("active"), 150);
         document.getElementById(destinationId).scrollIntoView({ behavior: "smooth" });
     };
 
-    render() {
-        return (
-            <div className="component-wrap">
-                {this.state.openPane && (
-                    <div
-                        className="backdrop"
-                        onClick={() => this.togglePane(this.state.openPane)}
-                    ></div>
-                )}
-                <Header
-                    togglePane={this.togglePane}
-                    openPane={this.state.openPane}
-                    scrollTo={this.scrollTo}
-                />
-                <Hero />
-                <WorkLog />
-                <Toolset togglePane={this.togglePane} openPane={this.state.openPane} />
-                <Projects togglePane={this.togglePane} openPane={this.state.openPane} />
-                <Contact />
-                <Footer />
-            </div>
-        );
-    }
-}
+    return (
+        <div className="component-wrap">
+            {openPane && <div className="backdrop" onClick={() => togglePane(openPane)}></div>}
+            <Header togglePane={togglePane} openPane={openPane} scrollTo={scrollTo} />
+            <Hero />
+            <WorkLog />
+            <Toolset togglePane={togglePane} openPane={openPane} />
+            <Projects togglePane={togglePane} openPane={openPane} />
+            <Contact />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;

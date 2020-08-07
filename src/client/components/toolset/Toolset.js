71fe2f4897509fafb850ebc83dd195fa4a9fb6a4
daisho <daishokomiyama@gmail.com>
2020-08-07 12:13:24 -0400

    Converting class into function component

--- src/client/components/toolset/Toolset.js
+++ src/client/components/toolset/Toolset.js
@@ -1,36 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { toolset, filterItems } from "./toolset-data.js";
 import icons from "../../svg/devIcons";
 import "./toolset.css";
 
-class Toolset extends Component {
-    state = {
-        filterTerms: [],
-        js: false,
-        markup: false,
-        graphicsEditor: false,
-        tools: false,
-        platform: false,
-        database: false,
-        textEditor: false
-    };
+const Toolset = ({ togglePane, openPane }) => {
+    const [filterTerms, setFilterTerms] = useState([]);
 
-    handleChange = (type) => {
-        const name = event.target.name;
-        this.setState({ [name]: event.target.checked });
-        const copy = [...this.state.filterTerms];
+    const handleChange = (type) => {
+        const copy = [...filterTerms];
         if (event.target.checked) {
-            this.setState((prevState) => ({ filterTerms: [...prevState.filterTerms, type] }));
+            setFilterTerms((prevFilterTerms) => [...prevFilterTerms, type]);
         } else {
-            const index = this.state.filterTerms.indexOf(type);
+            const index = filterTerms.indexOf(type);
             if (index >= 0) {
                 copy.splice(index, 1);
-                this.setState({ filterTerms: copy });
+                setFilterTerms(copy);
             }
         }
     };
 
-    renderStars = (num) => {
+    const renderStars = (num) => {
         let str = "";
         for (let i = 0; i < num; i++) {
             str += String.fromCharCode(9733);
@@ -38,72 +27,64 @@ class Toolset extends Component {
         return str;
     };
 
-    render() {
-        return (
-            <section id="toToolset" className="section is-toolset">
-                <div className="content-wrap is-side-by-side">
-                    <div className="left-side">
-                        <div className="title-group has-filters">
-                            <h2 className="title">Toolset</h2>
-                            <p className="subtitle font-large">
-                                My current toolset includes JavaScript and other various frameworks,
-                                libraries and technologies related to it.
-                                <br />
-                                <span className="star">{String.fromCharCode(9733)}</span> ==
-                                experience
-                            </p>
-                        </div>
-                        <fieldset
-                            className={`filter-section ${
-                                this.props.openPane === "tool" ? "active" : ""
-                            }`}
+    return (
+        <section id="toToolset" className="section is-toolset">
+            <div className="content-wrap is-side-by-side">
+                <div className="left-side">
+                    <div className="title-group has-filters">
+                        <h2 className="title">Toolset</h2>
+                        <p className="subtitle font-large">
+                            My current toolset includes JavaScript and other various frameworks,
+                            libraries and technologies related to it.
+                            <br />
+                            <span className="star">{String.fromCharCode(9733)}</span> == experience
+                        </p>
+                    </div>
+                    <fieldset className={`filter-section ${openPane === "tool" ? "active" : ""}`}>
+                        <button
+                            className="toggle-filter outline-button"
+                            onClick={() => togglePane("tool")}
                         >
-                            <button
-                                className="toggle-filter outline-button"
-                                onClick={() => this.props.togglePane("tool")}
-                            >
-                                Filters
-                            </button>
-                            <legend>(OR) Filters</legend>
-                            <div className="filters">
-                                {filterItems.map((item) => (
-                                    <label
-                                        key={item.type}
-                                        className={`tag ${this.state[item.type] ? "active" : ""}`}
-                                    >
-                                        <input
-                                            type="checkbox"
-                                            name={item.type}
-                                            checked={this.state.type}
-                                            onChange={() => this.handleChange(item.type)}
-                                        />
-                                        <span className="dummy"></span>
-                                        {item.name}
-                                    </label>
-                                ))}
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div className="toolset right-side">
-                        {toolset
-                            .filter((tool) => {
-                                return this.state.filterTerms.length
-                                    ? this.state.filterTerms.includes(tool.genre)
-                                    : true;
-                            })
-                            .map((tool) => (
-                                <div key={tool.id} className="tool">
-                                    {icons[tool.id]}
-                                    <span className="separator"></span>
-                                    <span className="stars">{this.renderStars(tool.stars)}</span>
-                                    <p className="tool-name">{tool.name}</p>
-                                </div>
+                            Filters
+                        </button>
+                        <legend>(OR) Filters</legend>
+                        <div className="filters">
+                            {filterItems.map((item) => (
+                                <label
+                                    key={item.type}
+                                    className={`tag ${
+                                        filterTerms.includes(item.type) ? "active" : ""
+                                    }`}
+                                >
+                                    <input
+                                        type="checkbox"
+                                        name={item.type}
+                                        onChange={() => handleChange(item.type)}
+                                    />
+                                    <span className="dummy"></span>
+                                    {item.name}
+                                </label>
                             ))}
-                    </div>
+                        </div>
+                    </fieldset>
                 </div>
-            </section>
-        );
-    }
-}
+                <div className="toolset right-side">
+                    {toolset
+                        .filter((tool) => {
+                            return filterTerms.length ? filterTerms.includes(tool.genre) : true;
+                        })
+                        .map((tool) => (
+                            <div key={tool.id} className="tool">
+                                {icons[tool.id]}
+                                <span className="separator"></span>
+                                <span className="stars">{renderStars(tool.stars)}</span>
+                                <p className="tool-name">{tool.name}</p>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default Toolset;

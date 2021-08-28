8073be3aae167f6d3ecfb39ee0918392430110b1
daisho <daishokomiyama@gmail.com>
2021-08-28 15:47:19 -0400

    Evaluate the skillset

--- src/client/components/toolset/toolset-data.js
+++ src/client/components/toolset/toolset-data.js
@@ -1,147 +1,147 @@
 export const toolset = [
-	{
-		id: 0,
-		name: "javascript",
-		genre: "js",
-		stars: 5
-	},
-	{
-		id: 1,
-		name: "typescript",
-		genre: "js",
-		stars: 3
-	},
-	{
-		id: 2,
-		name: "nodejs",
-		genre: "js",
-		stars: 3
-	},
-	{
-		id: 3,
-		name: "react",
-		genre: "js",
-		stars: 4
-	},
-	{
-		id: 4,
-		name: "angular",
-		genre: "js",
-		stars: 4
-	},
-	{
-		id: 5,
-		name: "html",
-		genre: "markup",
-		stars: 5
-	},
-	{
-		id: 6,
-		name: "handlebars",
-		genre: "markup",
-		stars: 4
-	},
-	{
-		id: 7,
-		name: "css",
-		genre: "markup",
-		stars: 5
-	},
-	{
-		id: 8,
-		name: "sass",
-		genre: "markup",
-		stars: 5
-	},
-	{
-		id: 9,
-		name: "less",
-		genre: "markup",
-		stars: 5
-	},
-	{
-		id: 10,
-		name: "photoshop",
-		genre: "graphicsEditor",
-		stars: 4
-	},
-	{
-		id: 11,
-		name: "gimp",
-		genre: "graphicsEditor",
-		stars: 3
-	},
-	{
-		id: 12,
-		name: "inkscape",
-		genre: "graphicsEditor",
-		stars: 3
-	},
-	{
-		id: 13,
-		name: "git",
-		genre: "tools",
-		stars: 4
-	},
-	{
-		id: 14,
-		name: "webpack",
-		genre: "tools",
-		stars: 3
-	},
-	{
-		id: 15,
-		name: "gulp",
-		genre: "tools",
-		stars: 3
-	},
-	{
-		id: 16,
-		name: "mongodb",
-		genre: "database",
-		stars: 2
-	},
-	{
-		id: 17,
-		name: "ubuntu",
-		genre: "platform",
-		stars: 4
-	},
-	{
-		id: 18,
-		name: "vim",
-		genre: "textEditor",
-		stars: 4
-	}
+  {
+    id: 0,
+    name: "javascript",
+    genre: "js",
+    stars: 5,
+  },
+  {
+    id: 1,
+    name: "typescript",
+    genre: "js",
+    stars: 3,
+  },
+  {
+    id: 2,
+    name: "nodejs",
+    genre: "js",
+    stars: 4,
+  },
+  {
+    id: 3,
+    name: "react",
+    genre: "js",
+    stars: 4,
+  },
+  {
+    id: 4,
+    name: "angular",
+    genre: "js",
+    stars: 3,
+  },
+  {
+    id: 5,
+    name: "html",
+    genre: "markup",
+    stars: 5,
+  },
+  {
+    id: 6,
+    name: "handlebars",
+    genre: "markup",
+    stars: 4,
+  },
+  {
+    id: 7,
+    name: "css",
+    genre: "markup",
+    stars: 5,
+  },
+  {
+    id: 8,
+    name: "sass",
+    genre: "markup",
+    stars: 5,
+  },
+  {
+    id: 9,
+    name: "less",
+    genre: "markup",
+    stars: 5,
+  },
+  {
+    id: 10,
+    name: "photoshop",
+    genre: "graphicsEditor",
+    stars: 4,
+  },
+  {
+    id: 11,
+    name: "gimp",
+    genre: "graphicsEditor",
+    stars: 3,
+  },
+  {
+    id: 12,
+    name: "inkscape",
+    genre: "graphicsEditor",
+    stars: 3,
+  },
+  {
+    id: 13,
+    name: "git",
+    genre: "tools",
+    stars: 4,
+  },
+  {
+    id: 14,
+    name: "webpack",
+    genre: "tools",
+    stars: 3,
+  },
+  {
+    id: 15,
+    name: "gulp",
+    genre: "tools",
+    stars: 3,
+  },
+  {
+    id: 16,
+    name: "mongodb",
+    genre: "database",
+    stars: 2,
+  },
+  {
+    id: 17,
+    name: "ubuntu",
+    genre: "platform",
+    stars: 4,
+  },
+  {
+    id: 18,
+    name: "vim",
+    genre: "textEditor",
+    stars: 4,
+  },
 ];
 
 export const filterItems = [
-	{
-		name: "JavaScript",
-		type: "js"
-	},
-	{
-		name: "Markup",
-		type: "markup"
-	},
-	{
-		name: "Graphics Editor",
-		type: "graphicsEditor"
-	},
-	{
-		name: "Tools",
-		type: "tools"
-	},
-	{
-		name: "Platform",
-		type: "platform"
-	},
-	{
-		name: "Database",
-		type: "database"
-	},
-	{
-		name: "Text Editor",
-		type: "textEditor"
-	}
+  {
+    name: "JavaScript",
+    type: "js",
+  },
+  {
+    name: "Markup",
+    type: "markup",
+  },
+  {
+    name: "Graphics Editor",
+    type: "graphicsEditor",
+  },
+  {
+    name: "Tools",
+    type: "tools",
+  },
+  {
+    name: "Platform",
+    type: "platform",
+  },
+  {
+    name: "Database",
+    type: "database",
+  },
+  {
+    name: "Text Editor",
+    type: "textEditor",
+  },
 ];

1aea95b2dc7142b10575bd051aa8f7f3d36cfd9a
Daisho <daishokomiyama@gmail.com>
2021-10-17 14:34:35 -0400

    Convert Toast to typescript

---
+++ src/client/components/toast/Toast.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent, useEffect } from 'react'
+import { DURATION } from './constants'
+
+interface IProps {
+  message: string
+  kind: string
+}
+
+const Toast: FunctionComponent<IProps> = ({ message, kind }) => {
+  const timeoutIds = []
+
+  useEffect(() => {
+    const toastElements = document.getElementsByClassName('toast-container')
+    timeoutIds.push(
+      setTimeout(() => {
+        toastElements[0].remove()
+      }, DURATION)
+    )
+
+    return () => {
+      while (timeoutIds.length) {
+        clearTimeout(timeoutIds.shift())
+      }
+    }
+  }, [])
+
+  return <div className={`toast ${kind}`}>{message}</div>
+}
+
+export default Toast

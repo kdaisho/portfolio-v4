902501194050a4df77c2ee8ed771198ed3cb70b5
Daisho Komiyama <daishokomiyama@gmail.com>
2021-12-26 18:29:09 -0500

    Add experiment for accepting POST request from quebec3.com

--- src/server/server.js
+++ src/server/server.js
@@ -15,6 +15,26 @@ app.get('*.js', function (req, res, next) {
 })
 app.use(express.static('dist'))
 app.use(express.json({ limit: '4kb' }))
+
+// ========= Experiment start
+// const corsOrigin = process.env.NODE_ENV === 'development' ? LOCALHOST : DOMAIN
+// app.use(express.json({ limit: '8kb' }))
+
+app.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', corsOrigin)
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST')
+    return res.status(200).json({})
+  }
+  next()
+})
+// ========= Experiment end
+
 app.post('/send', mail.sendMessage)
 
 app.listen(process.env.PORT, () => {

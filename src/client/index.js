f74fe9be0cd085dd95ded6392749b98b9edfcfd5
Daisho Komiyama <daishokomiyama@gmail.com>
2022-01-20 23:33:25 -0500

    Add analytics code using react-ga

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import App from "./App";
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './App'
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = 'UA-49713403-4'
+ReactGA.initialize(TRACKING_ID)
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root"),
-);
+  document.getElementById('root')
+)
